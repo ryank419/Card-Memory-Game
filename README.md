6f50cc992059aa428d2cb981953e74764497c453
ryank419 <ryankissoon05@gmail.com>
2026-03-27 18:35:42 -0400

    Add functionality and more assets
    - Added visual feedback for cards on hover
    - Added sound effects for hovering over or flipping a card

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Card Memory Game
 My first web project. A simple browser-based card matching game built with JavaScript.
+
+**Card Assets:** *Pixel Art Cards* by Lempf ([OpenGameArt](https://opengameart.org/content/pixel-art-cards))
---
+++ script.js
@@ -0,0 +1,88 @@
+let hoveredCard = null;
+const cardContainer = document.getElementById('cards');
+const cardFlipSound = new Audio('sfx/card-flip.wav');
+const cardHoverSound = new Audio('sfx/card-hover.wav');
+const addCardButton = document.getElementById('addCardButton');
+
+const availableCards = ["hearts-ace", "hearts-2", "hearts-3", "hearts-4", "hearts-5", "hearts-6", "hearts-7", "hearts-8", "hearts-9", "hearts-10", "hearts-jack", "hearts-queen", "hearts-king",
+                        "diamonds-ace", "diamonds-2", "diamonds-3", "diamonds-4", "diamonds-5", "diamonds-6", "diamonds-7", "diamonds-8", "diamonds-9", "diamonds-10", "diamonds-jack", "diamonds-queen", "diamonds-king",
+                        "clubs-ace", "clubs-2", "clubs-3", "clubs-4", "clubs-5", "clubs-6", "clubs-7", "clubs-8", "clubs-9", "clubs-10", "clubs-jack", "clubs-queen", "clubs-king",
+                        "spades-ace", "spades-2", "spades-3", "spades-4", "spades-5", "spades-6", "spades-7", "spades-8", "spades-9", "spades-10", "spades-jack", "spades-queen", "spades-king"];
+
+let chosenCards = [];
+
+addCardButton.addEventListener('click', createCards);
+
+document.addEventListener('DOMContentLoaded', () => {
+    choosePairs(5);
+    createCards();
+});
+
+
+function createCards() {
+
+    // Clear container and create new card elements based on chosenCards
+    cardContainer.innerHTML = '';
+    for (let i = 0; i < chosenCards.length; i++) {
+        cardContainer.innerHTML += `
+        <span class="card" data-card-id="${chosenCards[i]}">
+            <img src="images/card-back.png" class="card-img">
+        </span>
+        <span class="card" data-card-id="${chosenCards[i]}">
+            <img src="images/card-back.png" class="card-img">
+        </span>
+    `;
+    }
+
+    // Shuffle cards using Fisher-Yates algorithm
+    const elements = Array.from(cardContainer.children);
+
+    for (let i = elements.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [elements[i], elements[j]] = [elements[j], elements[i]];
+    }
+    elements.forEach(el => cardContainer.appendChild(el));
+
+    // Add event listeners to cards
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.addEventListener('click', () => flipCard(card));
+        card.addEventListener('mouseover', () => onCardHover(card));
+        card.addEventListener('mouseout', () => onCardLeave(card));
+    });
+}
+
+function choosePairs(pairs) {
+    chosenCards = [];
+    pairs = Number(pairs) || 5;
+    for (let i = 0; i < pairs; i++) {
+        const chosenPair = availableCards[Math.floor(Math.random() * availableCards.length)];
+        while (chosenCards[i] !== chosenPair) {
+            chosenCards[i] = chosenPair;
+        }
+    }
+}
+
+function flipCard(card) {
+    card.lastElementChild.classList.toggle('flipped');
+    cardFlipSound.cloneNode().play();
+}
+
+function setCardsPerRow(n) {
+    const val = Number(n) || 5;
+    cardContainer.style.setProperty('--cards-per-row', val);
+}
+
+function onCardHover(card) {
+    if (hoveredCard !== card) {
+        hoveredCard = card;
+        cardHoverSound.cloneNode().play();
+    }
+    console.log(`Hovered card ID: ${card.dataset.cardId}`);
+}
+
+function onCardLeave(card) {
+    if (hoveredCard === card) {
+        hoveredCard = null;
+    }
+}
